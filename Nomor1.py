fc1d44bbac94711111f1ea911d332cd7dfb4fe5a
MeisyaInjilia <90380351+MeisyaInjilia@users.noreply.github.com>
2021-09-21 19:03:16 +0700

    Add files via upload

---
+++ Nomor1.py
@@ -0,0 +1,26 @@
+print('Meisya Injilia Karmelita Kuera')
+print('2008878')
+print('================')
+
+
+class Segitiga:
+    def __init__ (self, T,i ):
+        self.T=T
+        self.i=i
+
+    def perhitungan1(self):
+        for self.i in range(1, self.T+1):
+            print((self.T-self.i)*"*")
+
+    def perhitungan2(self):
+        for self.i in range(1, self.T+1):
+            print((self.T-self.i)*" "+(self.T)*"*")
+
+Segitiga_terbalik = Segitiga(
+    T=6,
+    i=0
+)
+Segitiga_terbalik.perhitungan1()
+print("================")
+print("")
+Segitiga_terbalik.perhitungan2()
