ccb24cded6edf7a40531bb8011e9197cb4ea7290
MeisyaInjilia <90380351+MeisyaInjilia@users.noreply.github.com>
2021-09-21 18:01:50 +0700

    Add files via upload

---
+++ LuasSegitiga.py
@@ -0,0 +1,16 @@
+print("Meisya Injilia Karmelita Kuera")
+print("2008878")
+
+print("Menghitung Luas Segitiga")
+
+alas = int (input("Masukkan alas :"))
+tinggi = int (input("Masukkan tinggi :"))
+
+
+if 0 < alas <100 and 0 < tinggi < 100 :
+
+     luas = int (0.5*alas*tinggi)
+     print ("Luas segitiga adalah ",luas)
+     
+else :
+	print ("MASUKKAN ANGKA ANTARA 0 - 100")
